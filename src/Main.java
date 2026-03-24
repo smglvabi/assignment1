182ba41b32407a37c6bdcfc5b9635b034df6c788
abilmansursmagulov <smglvabi@gmail.com>
2026-03-24 22:51:44 +0500

    task6

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,23 @@ import java.util.Scanner;
 
 public class Main {
 
-    static int fib(int n) {
+    static int power(int a, int n) {
 
         if (n == 0) {
-            return 0;
-        }
-
-        if (n == 1) {
             return 1;
         }
 
-        return fib(n - 1) + fib(n - 2);
+        return a * power(a, n - 1);
     }
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
+        int a = sc.nextInt();
         int n = sc.nextInt();
 
-        System.out.println(fib(n));
+        System.out.println(power(a, n));
 
     }
 }
