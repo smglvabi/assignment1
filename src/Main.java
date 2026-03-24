00e94ac0ab0a8556f71961136c68cd28323fb594
abilmansursmagulov <smglvabi@gmail.com>
2026-03-24 21:50:52 +0500

    task2

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,33 @@
 import java.util.Scanner;
 
 public class Main {
-    static void printDigits(int n) {
-        if (n < 10) {
-            System.out.println(n);
+
+    static void readArray(int[] arr, int index, Scanner sc) {
+        if (index == arr.length) {
             return;
         }
+        arr[index] = sc.nextInt();
+        readArray(arr, index + 1, sc);
+    }
 
-        printDigits(n / 10);
-        System.out.println(n % 10);
+    static int sum(int[] arr, int n) {
+        if (n == 0) {
+            return 0;
+        }
+        return sum(arr, n - 1) + arr[n - 1];
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
-        printDigits(n);
+        int[] arr = new int[n];
+
+        readArray(arr, 0, sc);
+
+        int total = sum(arr, n);
+        double average = (double) total / n;
+
+        System.out.println(average);
     }
 }
