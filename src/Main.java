99c2dcfd1455d1e7b4f0fba2558238cdaa4f4e7c
abilmansursmagulov <smglvabi@gmail.com>
2026-03-24 22:36:11 +0500

    task5

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,26 @@ import java.util.Scanner;
 
 public class Main {
 
-    static int factorial(int n) {
-        if (n == 0 || n == 1) {
+    static int fib(int n) {
+
+        if (n == 0) {
+            return 0;
+        }
+
+        if (n == 1) {
             return 1;
         }
 
-        return n * factorial(n - 1);
+        return fib(n - 1) + fib(n - 2);
     }
 
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
 
-        System.out.println(factorial(n));
+        System.out.println(fib(n));
+
     }
 }
