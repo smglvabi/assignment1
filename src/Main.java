086c0f3ab37e95c755cb2814acea0f2c5b5edfa9
abilmansursmagulov <smglvabi@gmail.com>
2026-03-24 23:22:16 +0500

    task9 - final

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,13 @@ import java.util.Scanner;
 
 public class Main {
 
-    static int countUpper(String s, int index) {
+    static int countChars(String s, int index) {
 
         if (index == s.length()) {
             return 0;
         }
 
-        char c = s.charAt(index);
-
-        if (Character.isUpperCase(c)) {
-            return 1 + countUpper(s, index + 1);
-        }
-
-        return countUpper(s, index + 1);
+        return 1 + countChars(s, index + 1);
     }
 
     public static void main(String[] args) {
@@ -23,7 +17,7 @@ public class Main {
 
         String s = sc.nextLine();
 
-        System.out.println(countUpper(s, 0));
+        System.out.println(countChars(s, 0));
 
     }
 }
