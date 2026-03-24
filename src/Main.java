3229a03d897ed9460974b8c4dc612c90a976008c
abilmansursmagulov <smglvabi@gmail.com>
2026-03-24 22:27:34 +0500

    task4

--- src/Main.java
+++ src/Main.java
@@ -2,34 +2,19 @@ import java.util.Scanner;
 
 public class Main {
 
-    static boolean isPrime(int n, int i) {
-        if (i == 1) {
-            return true;
+    static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
         }
 
-        if (n % i == 0) {
-            return false;
-        }
-
-        return isPrime(n, i - 1);
+        return n * factorial(n - 1);
     }
 
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
 
-        if (n <= 1) {
-            System.out.println("Composite");
-            return;
-        }
-
-        if (isPrime(n, n - 1)) {
-            System.out.println("Prime");
-        } else {
-            System.out.println("Composite");
-        }
-
+        System.out.println(factorial(n));
     }
 }
